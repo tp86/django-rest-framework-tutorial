4b46b9cdefbc5e6bf856b9ef7d4c83131c4f58d8
tp86 <tom0palka+tp86@gmail.com>
2021-07-21 14:46:58 +0000

    Use mixins

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,55 +1,34 @@
-from django.http import Http404
-from rest_framework.decorators import api_view
-from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
+from rest_framework import generics, mixins
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-class SnippetList(APIView):
+class SnippetList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
     """
     List all code snippets, or create a new snippet.
     """
 
-    def get(self, request: Request, format=None) -> Response:
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
-    def post(self, request: Request, format=None) -> Response:
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    get = mixins.ListModelMixin.list
+    post = mixins.CreateModelMixin.create
 
 
-class SnippetDetail(APIView):
+class SnippetDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
     """
     Retrieve, update or delete a code snippet.
     """
 
-    def get_object(self, pk: int) -> Snippet:
-        try:
-            return Snippet.objects.get(pk=pk)
-        except Snippet.DoesNotExist:
-            raise Http404
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
-    def get(self, request: Request, pk: int, format=None) -> Response:
-        serializer = SnippetSerializer(self.get_object(pk))
-        return Response(serializer.data)
-
-    def put(self, request: Request, pk: int, format=None) -> Response:
-        serializer = SnippetSerializer(self.get_object(pk), data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request: Request, pk: int, format=None) -> Response:
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    get = mixins.RetrieveModelMixin.retrieve
+    put = mixins.UpdateModelMixin.update
+    delete = mixins.DestroyModelMixin.destroy
