c7698583891721460facb74c0bbf5f678dd3a776
tp86 <tom0palka+tp86@gmail.com>
2021-07-22 08:24:52 +0000

    Add user login views

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from snippets import views
@@ -11,3 +11,7 @@ urlpatterns = [
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls'))
+]
