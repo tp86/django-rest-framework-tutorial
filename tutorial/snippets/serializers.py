f26ea60f777863b858ba91a50e31cd08c14fb12c
tp86 <tom0palka+tp86@gmail.com>
2021-07-22 07:53:42 +0000

    Update serializer to include username when reading

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -4,9 +4,11 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
 
 
 class UserSerializer(serializers.ModelSerializer):
